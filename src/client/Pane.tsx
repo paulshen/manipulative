815a95c4e7bf281269f20983ab16de60ebd7497e
Paul Shen <paulshen0@gmail.com>
2020-11-29 12:44:16 -0800

    draggable Pane

---
+++ src/client/Pane.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef } from "react";
+import { css } from "@emotion/react";
+
+function Pane({ children }: { children: React.ReactNode }) {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const headerRef = useRef<HTMLDivElement>(null);
+  const offsetRef = useRef<[x: number, y: number]>([0, 0]);
+  const onUnmountRef = useRef<Function>();
+
+  function onHeaderMouseDown(e: React.MouseEvent) {
+    let lastPosition = [e.nativeEvent.screenX, e.nativeEvent.screenY];
+    function onMouseMove(e: MouseEvent) {
+      debugger;
+      const [lastX, lastY] = lastPosition;
+      const deltaX = e.screenX - lastX;
+      const deltaY = e.screenY - lastY;
+      offsetRef.current[0] += deltaX;
+      offsetRef.current[1] += deltaY;
+      rootRef.current!.style.transform = `translate3d(${offsetRef.current[0]}px, ${offsetRef.current[1]}px, 0)`;
+      lastPosition = [e.screenX, e.screenY];
+    }
+    function cleanup() {
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+    }
+    function onMouseUp() {
+      cleanup();
+    }
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("mouseup", onMouseUp);
+    onUnmountRef.current = cleanup;
+  }
+
+  useEffect(() => {
+    return () => {
+      if (onUnmountRef.current !== undefined) {
+        onUnmountRef.current();
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      css={css`
+        font-family: "SF Mono";
+        font-size: 12px;
+        position: fixed;
+        top: 16px;
+        right: 16px;
+        width: 320px;
+        background-color: #ffffff;
+        border-radius: 2px;
+        overflow: hidden;
+      `}
+      ref={rootRef}
+    >
+      <div
+        css={css`
+          background-color: #f0f0f0;
+          color: #808080;
+          padding: 8px 12px;
+          font-size: 10px;
+          letter-spacing: 0.2px;
+          text-transform: uppercase;
+          cursor: move;
+        `}
+        onMouseDown={onHeaderMouseDown}
+        ref={headerRef}
+      >
+        manipulative
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export default Pane;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,7 @@ import {} from "@emotion/react/types/css-prop";
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import create from "zustand";
+import Pane from "./Pane";
 
 type CallsiteValue = {
   value: string;
@@ -38,34 +39,28 @@ function Inspector() {
     return null;
   }
   return (
-    <div
-      css={css`
-        position: fixed;
-        font-family: "SF Mono";
-        font-size: 12px;
-        top: 0;
-        right: 0;
-        width: 256px;
-        padding: 12px 16px;
-        background-color: #ffffff;
-
-        button {
-          font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-          font-size: 12px;
-        }
-      `}
-    >
+    <Pane>
       <div
         css={css`
-          font-size: 10px;
-          letter-spacing: 0.2px;
-          margin-bottom: 8px;
-          text-transform: uppercase;
+          padding: 12px 12px;
+
+          button {
+            background-color: #808080;
+            border: 0;
+            border-radius: 2px;
+            color: #ffffff;
+            cursor: pointer;
+            font-family: "SF Mono";
+            font-size: 10px;
+            letter-spacing: 0.2px;
+            padding: 6px 10px;
+            text-transform: uppercase;
+            :hover {
+              background-color: #404040;
+            }
+          }
         `}
       >
-        manipulative
-      </div>
-      <div>
         {Object.keys(callsites).map((location) => {
           const callsite = callsites[location];
           const [filePath, position] = location.split(":");
@@ -105,7 +100,14 @@ function Inspector() {
                   </a>
                 </div>
                 {callsite.codeLine !== undefined ? (
-                  <div>{callsite.codeLine}</div>
+                  <div
+                    css={css`
+                      color: #b0b0b0;
+                      white-space: nowrap;
+                    `}
+                  >
+                    {callsite.codeLine}
+                  </div>
                 ) : null}
               </div>
               <div>
@@ -130,37 +132,37 @@ function Inspector() {
             </div>
           );
         })}
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            const updates = [];
-            for (const location in callsites) {
-              const [fileName, position] = location.split(":");
-              updates.push({
-                fileName,
-                position: parseInt(position),
-                value: callsites[location].value,
-              });
-            }
-            fetch(
-              `http://localhost:${
-                process.env.REACT_APP_MANIPULATIVE_PORT ?? 3001
-              }/commit`,
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ updates }),
+        <div>
+          <button
+            onClick={() => {
+              const updates = [];
+              for (const location in callsites) {
+                const [fileName, position] = location.split(":");
+                updates.push({
+                  fileName,
+                  position: parseInt(position),
+                  value: callsites[location].value,
+                });
               }
-            );
-          }}
-        >
-          commit
-        </button>
+              fetch(
+                `http://localhost:${
+                  process.env.REACT_APP_MANIPULATIVE_PORT ?? 3001
+                }/commit`,
+                {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({ updates }),
+                }
+              );
+            }}
+          >
+            commit
+          </button>
+        </div>
       </div>
-    </div>
+    </Pane>
   );
 }
 
